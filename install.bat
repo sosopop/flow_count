9e2ec5a26db31707fb4ec4107538fa757df4bff2
Sara Sawayn <cabbage1102@gmail.com>
2024-03-13 10:52:31 +0800

    增加依赖包安装脚本

---
+++ install.bat
@@ -0,0 +1,12 @@
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install opencv-contrib-python
+pip install psutil
+pip install matplotlib
+pip install pyyaml
+pip install tqdm
+pip install requests
+pip install pandas
+pip install hub_sdk
+pip install influxdb_client
+pip install scipy
+pip install py-cpuinfo
